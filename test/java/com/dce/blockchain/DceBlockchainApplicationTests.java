c526a09b7d57e2f6d2a79bd1fa46485b0a84fbee
yzqi <yongqi014@gmail.com>
2021-08-10 13:02:36 -0700

    upload Magchain

---
+++ test/java/com/dce/blockchain/DceBlockchainApplicationTests.java
@@ -0,0 +1,27 @@
+package java.com.dce.blockchain;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+class DceBlockchainApplicationTests {
+
+	@Before
+    public void init() {
+        System.out.println("开始单元测试-----------------");
+    }
+
+
+    @After
+    public void after() {
+        System.out.println("单元测试结束-----------------");
+    }
+
+}
